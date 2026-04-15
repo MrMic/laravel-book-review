0e4eaf2a1e467c475592dea06dcc2f93e7094027
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2026-04-15 10:16:53 +0200

    feat(book): ajoute le BookController et les routes RESTful
    Génère le contrôleur de ressource BookController avec toutes les
    méthodes CRUD. Enregistre la route resource `books` dans web.php.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+// ______________________________________________________________________
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ______________________________________________________________________
+Route::resource('books', BookController::class);
